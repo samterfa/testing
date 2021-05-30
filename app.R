8e4da8a98ff2cbb6a0d19466aecc5bef6bc310b3
Sam Terfa <sam.terfa@reemahealth.com>
2021-05-29 21:24:08 -0500

    Update app.R

--- app.R
+++ app.R
@@ -7,12 +7,13 @@ shinyApp(
     textOutput("secret2")
   ),
   server = function(input, output) {
-    print(dir.exists('/myvolume'))
+    message(dir.exists('/myvolume'))
+    message(file.exists('/myvolume/secret2'))
     files <- list.files('/myvolume')
     files <- paste(files, collapse = ', ')
     dirs <- list.dirs('/myvolume')
     dirs <- paste(dirs, collapse = ', ')
     output$secret <- renderText(paste0("files = ", files))
-    output$secret2 <- renderText(paste0("dirs = ", dirs))
+    output$secret2 <- renderText(readLines('/myvolume/secret2'))
   }
 )
